57e5a04b4d9c4167c427db98aa4b77670779deaf
mohmmad <azimi.jozani.mohammad@gmail.com>
2023-03-02 10:38:34 +0330

    organize my code ;)

--- 01/index.js
+++ 01/index.js
@@ -31,41 +31,23 @@ function isValidEmail(email){
   return re.test(String(email).toLowerCase())
 }
 
+function checkRequired(input){
+    input.forEach(element => {
+        console.log(element.value)
+        if(element.value.trim() === ''){
+            showError(element, `${element.id} is required!`)
+
+        }else{
+            showSeccess(element)
+        }
+
+    });
+}
 
 
 
 // Event Listener
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-
-    console.log(username.value)
-
-    if(username.value === ''){
-        showError(username, 'username is required!')
-    }else{
-        showSeccess(username)
-    }
-
-    if(email.value === ''){
-        showError(email, 'email is required!')
-    }else if(!isValidEmail(email.value)) {
-        showError(email, 'email is not valid')
-        
-    }
-    else{
-        showSeccess(email)
-    }
-
-    if(password.value === ''){
-        showError(password, 'password is required!')
-    }else{
-        showSeccess(password)
-    }
-
-    if(confirm.value === ''){
-        showError(confirm, 'confirm is required!')
-    }else{
-        showSeccess(confirm)
-    }
-
+    checkRequired([username, email, password, confirm])
 })
